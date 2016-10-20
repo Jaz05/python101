b1498e68239eb354142e7c5b94efd0fe28be9a1c
ITRESOURCES\Javier_Zardain <office_05@hotmail.com>
2016-10-20 12:07:37 -0300

    Correcciones ejercicio 2

--- 2.0-funciones/ejercicios_alumnos/Zardain, Javier/Ejercicios/Ej12.py
+++ 2.0-funciones/ejercicios_alumnos/Zardain, Javier/Ejercicios/Ej12.py
@@ -1,23 +1,16 @@
 '''
-Created on 7/9/2016
+Created on 20/10/2016
 
 @author: javier_zardain
 
 - Dado una lista de numeros calcular la sumatoria de dicho numero.
 '''
-
 lista = [1,2,3,4,5,6,7,8,9]
 
-def calcularSumatoria(lista,indice = 0):
-    x = 0
-    sumatoria = 0
-    while(x < indice):
-        sumatoria = sumatoria + lista[x]
-        x += 1
-    print("La sumatoria de " + str(indice) + " es " + str(sumatoria))
-    if(indice < len(lista)):
-        calcularSumatoria(lista,indice+1)
+def calcularSumatoria(lista):
+    if(len(lista) > 1):
+        return lista.pop()+calcularSumatoria(lista)
+    elif(len(lista) == 1):
+        return lista.pop()
 
-
-calcularSumatoria(lista)
-        
+print("La sumatoria de " + str(lista) + " es: " + str(calcularSumatoria(lista)))
--- 2.0-funciones/ejercicios_alumnos/Zardain, Javier/Ejercicios/Ej14.py
+++ 2.0-funciones/ejercicios_alumnos/Zardain, Javier/Ejercicios/Ej14.py
@@ -1,20 +1,18 @@
 '''
-Created on 8/9/2016
+Created on 20/10/2016
 
 @author: javier_zardain
 
 Dadas 2 listas con la misma cantidad de elementos intercalarlos.
 '''
 
-def cruzarListas(lista1, lista2, nuevaLista, indice=0):
-    x = int(len(nuevaLista)/2)
-    nuevaLista.append(lista1[indice])
-    nuevaLista.append(lista2[indice])
-    if x != len(lista1)-1:
-        cruzarListas(lista1, lista2, nuevaLista, indice+1)
+def cruzarListas(lista1, lista2):
+    if(len(lista1) > 1):
+        lista = [lista1.pop(),lista2.pop()]
+        return (cruzarListas(lista1, lista2)) + lista
+    elif(len(lista1) == 1):
+        return [lista1.pop(),lista2.pop()]
 
 lista1 = [1,2,3,4]
 lista2 = [9,8,7,6]
-nuevaLista = []
-cruzarListas(lista1, lista2, nuevaLista)
-print(nuevaLista)
+print(cruzarListas(lista1, lista2))
